977b0c56d3670badf8d25c2041a1db12ef2558db
hernandezalvarezsatwit <hernandezalvarezs@wit.edu>
2022-02-27 12:52:20 -0500

    Using method to facilitate process

--- app/src/main/java/edu/wit/mobileapp/webapp/FetchAnActivity.java
+++ app/src/main/java/edu/wit/mobileapp/webapp/FetchAnActivity.java
@@ -40,21 +40,11 @@ public class FetchAnActivity extends AsyncTask<Void, Void, String> {
     @Override
     protected String doInBackground(Void... voids) {
         try {
-            //Weather: https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=458514581d29fd64d5ba0ececf114335
-            //Stock: X5N8KPH4CUJ9C5YP
-            //weather 2: 5f0c4bbeb2294551b9431534222702
-
-            String weather = "http://api.openweathermap.org/data/2.5/find?q=Boston&units=imperial&appid=458514581d29fd64d5ba0ececf114335";
-            String w = "http://api.weatherapi.com/v1/current.json?key=5f0c4bbeb2294551b9431534222702&q=Boston&aqi=no";
-            //https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=TSLA&interval=5min&outputsize=full&apikey=X5N8KPH4CUJ9C5YP
-            //https://www.boredapi.com/api/activity
+            //Alpha Stock key: X5N8KPH4CUJ9C5YP
             String stock = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=TSLA&interval=5min&outputsize=full&apikey=X5N8KPH4CUJ9C5YP";
             URL url = new URL(stock);
-
-            //HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             InputStream inputStream = connection.getInputStream();
-
             if(inputStream == null) {
                 Log.v("myApp", "Not retrieved");
                 return "Data was not retrieved";
@@ -88,5 +78,92 @@ public class FetchAnActivity extends AsyncTask<Void, Void, String> {
                 e.printStackTrace();
             }
         }
+    }
+
+    private static String getStockData(String company) {
+        String stockData = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol="+company+"&interval=5min&outputsize=full&apikey=X5N8KPH4CUJ9C5YP";
+        Object obj = null;
+        try {
+            obj = new JSONParser().parse(getJsonFrom(stockData));
+        } catch (ParseException | IOException e) {
+            System.out.println("---> Problem parsing JSON of stockData or Buffer Reader error");
+            //Log.v("myApp", "Problem parsing JSON of stockData or Buffer Reader error");
+            e.printStackTrace();
+        }
+        org.json.simple.JSONObject jo = (org.json.simple.JSONObject) obj;
+        Map stockHeader = (HashMap) jo.get("Meta Data");
+        String symbol = stockHeader.get("2. Symbol").toString();
+        String last = stockHeader.get("3. Last Refreshed").toString();
+
+        Map stockPrices = (HashMap) jo.get("Time Series (5min)");
+        String lastData = stockPrices.get(last).toString();
+        Object obj2 = null;
+        try {
+            obj2 = new JSONParser().parse(lastData);
+        } catch (ParseException e) {
+            System.out.println("---> Problem parsing JSON of last prices of stock");
+            //Log.v("myApp", "Problem parsing JSON of last prices of stock");
+            e.printStackTrace();
+        }
+        org.json.simple.JSONObject refreshedData = (org.json.simple.JSONObject) obj2;
+        String open = "Not Available";
+        String temp = refreshedData.get("1. open").toString();
+        if(temp != null)
+            open = temp;
+
+        String close = "Not Available";
+        String temp2 = refreshedData.get("4. close").toString();
+        if(temp2 != null)
+            close = temp2;
+
+        StringBuilder data = new StringBuilder("Symbol: ");
+        data.append(symbol+"\n"); data.append("Open: "+open+"\n"); data.append("Close: "+close);
+        return data.toString();
+    }
+
+    private static String getJsonFrom(String link) throws IOException {
+        //Retrieve info from URL
+        URL url = null;
+        try {
+            url = new URL(link);
+        } catch (MalformedURLException e) {
+            System.out.println("---> URL not retrieved correctly. URL: "+link);
+            //Log.v("myApp", "URL not retrieved correctly. URL: "+link);
+            e.printStackTrace();
+        }
+
+        //HTTP Connect
+        HttpURLConnection connection = null;
+        try {
+            connection = (HttpURLConnection) url.openConnection();
+        } catch (IOException e) {
+            System.out.println("---> HTTP Connection Problem");
+            //Log.v("myApp", "HTTP Connection Problem");
+            e.printStackTrace();
+        }
+
+        //Get Input stream
+        InputStream inputStream = null;
+        try {
+            inputStream = connection.getInputStream();
+        } catch (IOException e) {
+            System.out.println("---> Input Stream Problem");
+            //Log.v("myApp", "Input Stream Problem");
+            e.printStackTrace();
+        }
+        if(inputStream == null) {
+            System.out.println("---> Data was not retrieved");
+            //Log.v("myApp", "Data was not retrieved"");
+            return "Data was not retrieved";
+        }
+
+        //Get information into string
+        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while((line = br.readLine()) != null)
+            sb.append(line);
+
+        return sb.toString();
     }
 }
